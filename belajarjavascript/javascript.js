45b20d1aaf23b1cb2b6f3fa3057407e381fa3d8d
JefriEkaRakhmansyah <140358080+JefriEkaRakhmansyah@users.noreply.github.com>
2023-09-01 10:11:33 +0700

    javascript

---
+++ belajarjavascript/javascript.js
@@ -0,0 +1,134 @@
+let a=2;
+let b=3;
+let c=a+b;
+console.log(c);
+
+3+2*4/2*3*3+2-5
+let e=2;
+let d=3;
+let f=4;
+let g=5;
+let h=((d+e)*f)/(e*d*d)+(e-g);
+console.log(h);
+
+
+
+
+let x=6;
+let y=5;
+
+if(x>y){
+    console.log("benar");
+ }else {
+    console.log("salah");
+ }
+
+let kkm=80;
+let nilai=60;
+
+if(nilai>kkm){
+    console.log("Lulus")
+} else{
+    console.log("Tidak Lulus")
+}
+
+let bulan=1;
+let tanggal=20;
+let zodiak="belum dibuat";
+if(bulan==1){
+    if(tanggal>0 && tanggal<20){
+        zodiak="aquarius";
+    }
+    if (tanggal>19 && tanggal<32){
+        zodiak="capricon";
+    }
+}
+if(bulan==2){
+    if(tanggal>0 && tanggal<19){
+        zodiak="gemini";
+    }
+    if(tanggal>18 && tanggal>29){
+        zodiak="taurus";
+    }
+}
+if(bulan==3){
+    if(tanggal>0 && tanggal<20){
+        zodiak="agos";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="yunus";
+    }
+}
+if(bulan==4){
+    if(tanggal>0 && tanggal<18){
+        zodiak="muklas";
+    }
+    if(tanggal>17 && tanggal<31){
+        zodiak="titiw";
+    }
+}
+if(bulan==5){
+    if(tanggal>0 && tanggal<19){
+        zodiak="buas";
+    }
+    if(tanggal>18 && tanggal<32){
+        zodiak="cuopu";
+    }
+}
+if(bulan==6){
+    if(tanggal>0 && tanggal<16){
+    zodiak="dapoo";
+    }
+    if(tanggal>15 && tanggal<31){
+    zodiak="gembeng";
+}
+}
+if(bulan==7){
+    if(tanggal>0 && tanggal <16){
+        zodiak="uranus";
+    }
+    if(tanggal>15 && tanggal<32){
+        zodiak="yesus";
+    }
+}
+if(bulan==8){
+    if(tanggal>0 && tanggal<20){
+        zodiak="abuy";
+    }
+    if(tanggal>19 && tanggal<31){
+        zodiak="asus";
+    }
+}
+if(bulan==9){
+    if(tanggal>0 && tanggal<21){
+        zodiak="yuda";
+    }
+    if(tanggal>20 && tanggal<32){
+        zodiak="budi";
+    }
+}
+if(bulan==10){
+    if(tanggal>0 && tanggal<22){
+        zodiak="meri";
+    }
+    if(tanggal>21 && tanggal<31){
+        zodiak="asus";
+    }
+}
+if(bulan==11){
+    if(tanggal>0 && tanggal<23){
+        zodiak="bugus";
+    }
+    if(tanggal>22 && tanggal<32){
+        zodiak="tugus";
+    }
+}
+if(bulan==12){
+    if(tanggal>0 && tanggal<18){
+        zodiak="siti";
+    }
+    if(tanggal>17 && tanggal<32){
+        zodiak="tikus";
+    }
+}
+console.log(zodiak);
